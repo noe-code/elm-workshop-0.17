9430c55d44ccb4e0b0e1630af80e2e2fa3be1501
Noemi Rodriguez <elmaildenoe@gmail.com>
2016-09-29 18:25:05 +0200

    Exercise 09 done

--- part09/tests/Tests.elm
+++ part09/tests/Tests.elm
@@ -18,11 +18,12 @@ all =
                         """{ "pizza": [] }"""
 
                     isErrorResult result =
-                        -- TODO return True if the given Result is an Err of some sort,
-                        -- and False if it is an Ok of some sort.
-                        --
-                        -- Result docs: http://package.elm-lang.org/packages/elm-lang/core/4.0.1/Result
-                        False
+                        case result of
+                            Err _ ->
+                                True
+
+                            Ok _ ->
+                                False
                 in
                     json
                         |> decodeString responseDecoder
@@ -31,7 +32,8 @@ all =
         , test "it successfully decodes a valid response" <|
             \() ->
                 """{ "items": [
-                    /* TODO: put JSON here! */
+                      { "id": 5, "full_name": "foo", "stargazers_count": 42 },
+                      { "id": 3, "full_name": "bar", "stargazers_count": 77 }
                  ] }"""
                     |> decodeString responseDecoder
                     |> Expect.equal
@@ -40,22 +42,14 @@ all =
                             , { id = 3, name = "bar", stars = 77 }
                             ]
                         )
-        , test "it decodes one SearchResult for each 'item' in the JSON" <|
-            \() ->
+        , fuzz (list int) "it decodes one SearchResult for each 'item' in the JSON" <|
+            \fuzzIds ->
                 let
-                    -- TODO convert this to a fuzz test that generates a random
-                    -- list of ids instead of this hardcoded list of three ids.
-                    --
-                    -- fuzz test docs: http://package.elm-lang.org/packages/project-fuzzball/test/2.0.1/Test#fuzz
-                    -- Fuzzer docs: http://package.elm-lang.org/packages/project-fuzzball/test/2.0.1/Fuzz
-                    ids =
-                        [ 12, 5, 76 ]
-
                     jsonFromId id =
                         """{"id": """ ++ toString id ++ """, "full_name": "foo", "stargazers_count": 42}"""
 
                     jsonItems =
-                        String.join ", " (List.map jsonFromId ids)
+                        String.join ", " (List.map jsonFromId fuzzIds)
 
                     json =
                         """{ "items": [""" ++ jsonItems ++ """] }"""
@@ -63,7 +57,7 @@ all =
                     case decodeString responseDecoder json of
                         Ok results ->
                             List.length results
-                                |> Expect.equal (List.length ids)
+                                |> Expect.equal (List.length fuzzIds)
 
                         Err err ->
                             Expect.fail ("JSON decoding failed unexpectedly: " ++ err)
